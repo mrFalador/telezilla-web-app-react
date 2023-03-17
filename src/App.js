09e99e97d11164eda956585d84bd25355bcfdaec
Tim <kaplunov@doczilla.ru>
2023-03-17 12:11:28 +0300

    Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import {useEffect} from "react";
-const tg = window.Telegram.WebApp;
 
 function App() {
 
-  useEffect(() => {
-    tg.ready()
-  }, [])
-  const onClose = () => {
-    tg.close()
-  }
+    useEffect(() => {
+        tg.ready()
+    }, [])
 
   return (
     <div className="App">
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect} from 'react';
+import Button from "../Button/Button";
+
+const Header = () => {
+
+    const tg = window.Telegram.WebApp;
+    const onClose = () => {
+        tg.close()
+    }
+
+    return (
+        <div className={'header'}>
+            <Button onClick={onClose()}>Закрыть</Button>
+            <span className={'userName'}>
+                {tg.initDataUnsafe?.user?.username}
+            </span>
+        </div>
+    );
+};
+
+export default Header;
